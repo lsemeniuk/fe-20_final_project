47c65e2fac803892c84ce553c476c2b8b3591e2f
LeoSam <leo.sam@ukr.net>
2021-04-08 11:55:19 +0300

    add basic client

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App" style={{width: '600px', margin: '0 auto', textAlign: 'center'}}>
+      <h1>HELLO!!!</h1>
+      <p>This is the first element created in OUR final project</p>
+    </div>
+  );
+}
+
+export default App;
--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    'mongodb+srv://fe-20_final_project:vSk5JxkURRmmpRzs@cluster0.yzjth.mongodb.net/fe-20final_project?retryWrites=true&w=majority',
-  secretOrKey: 'p8NBsoHTQrbf73X9yviT',
+    'mongodb+srv://fe-20_final_project:JdI0UsAjm2jMMF8v@cluster0.yzjth.mongodb.net/fe-20_final_project?retryWrites=true&w=majority',
+  secretOrKey: 'random very very secret string',
 };
