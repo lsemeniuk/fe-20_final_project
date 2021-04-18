201c035c303c87a2ff1aead6be4d173d37b0e987
LeoSam <leo.sam@ukr.net>
2021-04-19 01:59:24 +0300

    add modal

--- src/pages/Index/Index.js
+++ src/pages/Index/Index.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Modal from '../../components/modal/Modal/Modal';
 
 const Index = () => {
-  return <div>index</div>;
+  return (
+    <div>
+      <Modal>
+        <div>lorem sda ewrew fdgdrt c rtytrt rfghgf hf g</div>
+      </Modal>
+    </div>
+  );
 };
 
 export default Index;
