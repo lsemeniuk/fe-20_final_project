ca1fbce90bcdd0a83d49261e9dc3ad510c0739fc
Erkimbaev Timur <etimur808@gmail.com>
2021-04-19 22:12:31 +0300

    product item

---
+++ src/components/ProductItem/ProductItem.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import ProductItem from './ProductItem';
+
+describe('Test product item component', () => {
+  test('smoke item test', () => {
+    render(<ProductItem />);
+  });
+  // остальные тесты допишу потом, когда в компонент будут приходить какие-то пропсы
+});
