2309ac1f88bbf393af22a6f8bc87f8fa7ffa8cf3
Alexey Kudryashov <a.kudr74@gmail.com>
2021-04-20 15:52:57 +0300

    Some part of ProductScreen verstka...

--- src/pages/HomeScreen/HomeScreen.js
+++ src/pages/HomeScreen/HomeScreen.js
@@ -1,26 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import Button from '../../components/Button/Button';
-import CartModal from '../../components/modals/CartModal';
 import styles from './HomeScreen.module.scss';
 
 const HomeScreen = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const showModal = () => {
-    setIsOpen(true);
-  };
-  const hideModal = () => {
-    setIsOpen(false);
-  };
   return (
     <div className={styles.home}>
       <h1>First element of the project</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-      <Button onClick={showModal}>Купить часы</Button>
       <Link to="/product/currentID" className={styles.link}>
         To a selected Product Screen
       </Link>
-      <CartModal isOpen={isOpen} hideModal={hideModal} />
     </div>
   );
 };
