a7cb780b5781aa90bbfb120e7a74978e5be3e261
LeoSam <leo.sam@ukr.net>
2021-04-21 12:03:51 +0300

    add component button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,20 +1,37 @@
-import React from "react";
-import PropTypes from "prop-types";
-import styles from "./Button.module.scss";
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Button.module.scss';
 
-const Button = ({ variant, title, onClick, disabled }) => {
-  return (<>
-    <button className="button" style={styles} onClick={onClick} disabled={disabled}>
-      {title}
-    </button>
-  </>
+const Button = ({ variant, title, onClick, disabled = false }) => {
+  let className = '';
+
+  if (variant === 'outline') {
+    className = `${styles.button} ${styles.outline}`;
+  } else if (variant === 'special') {
+    className = `${styles.button} ${styles.examples} ${styles.special}`;
+  } else {
+    className = `${styles.button} ${styles.examples}`;
+  }
+
+  return (
+    <>
+      <button type='button' className={className} onClick={onClick} disabled={disabled}>
+        {title}
+      </button>
+    </>
   );
 };
 
 Button.propTypes = {
-  styles: PropTypes.object,
-  text: PropTypes.string.isRequired,
+  variant: PropTypes.string,
+  title: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  variant: '',
+  disabled: false,
 };
 
 export default Button;
