623cf602f3818a5382185c42cb08bb2819489008
Erkimbaev Timur <etimur808@gmail.com>
2021-04-21 10:55:45 +0300

    product item add

--- src/components/ProductItem/ProductItem.js
+++ src/components/ProductItem/ProductItem.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import style from './productItem.module.scss';
 
-const ProductItem = () => {
-  return <div>ProductItem</div>;
+const product = {
+  img: 'https://design109.horoshop.ua/content/images/25/240x240l85nn0/33197235775948.jpeg',
+  name: 'Смарт-часы SAMSUNG Galaxy Watch Active 2 40mm Aluminium Pink Gold',
+  currentPrice: '1200 грн',
+  previousPrice: '1500 грн',
+  isNew: true,
 };
 
+const ProductItem = () => {
+  const { img, isNew, name, currentPrice, previousPrice } = product;
+  return (
+    <div className={style.item}>
+      <img src={img} alt="watch" />
+      {isNew && <div className={style.newMessage}>Новинка</div>}
+      {/* вставил здесь ссылку потому что eslint ругается */}
+      <a href="https://horoshop.ua/ua/design/109/#desktop" title={name} className={style.name}>
+        {name}
+      </a>
+
+      <div className={style.priceSection}>
+        <p className={style.currentPrice}>{currentPrice}</p>
+        <p className={style.previousPrice}>
+          <s>{previousPrice}</s>
+        </p>
+      </div>
+
+      <div className={style.btnSection}>
+        <button type="button" className={style.buyBtn}>
+          Купить
+        </button>
+        <button type="button" className={style.favBtn}>
+          fav
+        </button>
+      </div>
+    </div>
+  );
+};
+
+ProductItem.defaultProps = {
+  name: 'Товар не найден',
+  isNew: false,
+};
 export default ProductItem;
