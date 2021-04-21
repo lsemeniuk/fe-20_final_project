22120d366a6d9856b82901ccd8ccf7011ffb1764
Erkimbaev Timur <etimur808@gmail.com>
2021-04-21 14:08:59 +0300

    product item add

--- src/components/ProductItem/ProductItem.js
+++ src/components/ProductItem/ProductItem.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import style from './productItem.module.scss';
+import Button from '../Button/Button';
 
 const product = {
-  img: 'https://design109.horoshop.ua/content/images/25/240x240l85nn0/33197235775948.jpeg',
+  img: ['https://design109.horoshop.ua/content/images/25/240x240l85nn0/33197235775948.jpeg'],
   name: 'Смарт-часы SAMSUNG Galaxy Watch Active 2 40mm Aluminium Pink Gold',
   currentPrice: '1200 грн',
   previousPrice: '1500 грн',
@@ -13,10 +14,10 @@ const ProductItem = () => {
   const { img, isNew, name, currentPrice, previousPrice } = product;
   return (
     <div className={style.item}>
-      <img src={img} alt='watch' />
+      <img src={img[0]} alt="watch" />
       {isNew && <div className={style.newMessage}>Новинка</div>}
       {/* вставил здесь ссылку потому что eslint ругается */}
-      <a href='https://horoshop.ua/ua/design/109/#desktop' title={name} className={style.name}>
+      <a href="https://horoshop.ua/ua/design/109/#desktop" title={name} className={style.name}>
         {name}
       </a>
 
@@ -28,13 +29,8 @@ const ProductItem = () => {
       </div>
 
       <div className={style.btnSection}>
-        {/* проставил type потому что eslint ругается */}
-        <button type='submit' className={style.buyBtn}>
-          Купить
-        </button>
-        <button type='submit' className={style.favBtn}>
-          fav
-        </button>
+        <Button type="button" variant="special" title="Купить" />
+        <Button type="button" variant="special" title="fav" />
       </div>
     </div>
   );
@@ -44,4 +40,11 @@ ProductItem.defaultProps = {
   name: 'Товар не найден',
   isNew: false,
 };
+// ProductItem.propTypes = {
+//   name: PropTypes.string,
+//   img: PropTypes.array,
+//   currentPrice: PropTypes.string,
+//   previousPrice: PropTypes.string,
+//   isNew: PropTypes.bool,
+// };
 export default ProductItem;
