95f20f29251707ac46c8bbfb18f31699d283158c
imperor <imperor.oktov@gmail.com>
2021-05-03 11:29:42 +0300

    select_bar_0

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AppRoutes from './routes/AppRoutes';
 import Footer from './components/Footer/Footer';
 import NavBar from './components/NavBar/NavBar';
+import MyFilterContainer from './components/SelectBar/MyFilterContainer/MyFilterContainer';
+import styles from './App.module.scss';
 
 function App() {
+  const [sort] = useState(true);
+  const [checkboxed] = useState(true);
   return (
     <div className='App'>
       <NavBar />
+      <MyFilterContainer sort={sort} />
+      <MyFilterContainer checkboxed={checkboxed} />
+
       <AppRoutes />
+
+      {/* <MyFilterContainer sort={sort} /> */}
+      <div className={styles.divs}>aaa</div>
       <Footer />
     </div>
   );
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
+import filtersReducer from './filter/reducer';
 import customerReducer from './customer/reducer';
 import productsReducer from './products/reducer';
 import catalogReducer from './catalog/reducer';
 
 const reducer = combineReducers({
+  filters: filtersReducer,
   customer: customerReducer,
   products: productsReducer,
   catalog: catalogReducer,
