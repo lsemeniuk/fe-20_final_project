73afdf809c4769d43eb8c94be97b40693ee32089
Alexey Kudryashov <a.kudr74@gmail.com>
2021-05-07 18:47:24 +0300

    список желаний работает...

--- src/store/favorites/actions.js
+++ src/store/favorites/actions.js
@@ -1,28 +1,34 @@
 import axios from 'axios';
-import { ADD_TO_FAVORITES, CLEAR_FAVORITES, REMOVE_FROM_FAVORITES, LOAD_FAVORITES, SET_ISLOADING } from './types';
+import {
+  FAVORITES_ADD_ITEM,
+  FAVORITES_CLEAR,
+  FAVORITES_LOAD,
+  FAVORITES_REMOVE_ITEM,
+  FAVORITES_SET_ISLOADING,
+} from './types';
 
 export const loadFavorites = () => dispatch => {
-  dispatch({ type: SET_ISLOADING, payload: true });
+  dispatch({ type: FAVORITES_SET_ISLOADING, payload: true });
   axios('/favorites-list.json').then(res => {
-    dispatch({ type: LOAD_FAVORITES, payload: res.data });
-    dispatch({ type: SET_ISLOADING, payload: false });
+    dispatch({ type: FAVORITES_LOAD, payload: res.data });
+    dispatch({ type: FAVORITES_SET_ISLOADING, payload: false });
   });
 };
 
 export const addToFavoritesAction = product => {
   return {
-    type: ADD_TO_FAVORITES,
+    type: FAVORITES_ADD_ITEM,
     payload: product,
   };
 };
 export const removeFromFavoritesAction = ID => {
   return {
-    type: REMOVE_FROM_FAVORITES,
+    type: FAVORITES_REMOVE_ITEM,
     payload: ID,
   };
 };
 export const clearFavoritesAction = () => {
   return {
-    type: CLEAR_FAVORITES,
+    type: FAVORITES_CLEAR,
   };
 };
--- src/store/favorites/reducer.js
+++ src/store/favorites/reducer.js
@@ -1,4 +1,10 @@
-import { ADD_TO_FAVORITES, CLEAR_FAVORITES, LOAD_FAVORITES, REMOVE_FROM_FAVORITES, SET_ISLOADING } from './types';
+import {
+  FAVORITES_ADD_ITEM,
+  FAVORITES_CLEAR,
+  FAVORITES_LOAD,
+  FAVORITES_REMOVE_ITEM,
+  FAVORITES_SET_ISLOADING,
+} from './types';
 
 const initialState = {
   isLoading: '',
@@ -7,27 +13,25 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case LOAD_FAVORITES:
+    case FAVORITES_LOAD:
       return { ...state, data: action.payload };
 
-    case SET_ISLOADING:
+    case FAVORITES_SET_ISLOADING:
       return { ...state, isLoading: action.payload };
 
-    case ADD_TO_FAVORITES: {
-      //! action.payload is Product!
+    case FAVORITES_ADD_ITEM: {
       const inFavs = state.data.find(item => item._id === action.payload._id);
 
       if (inFavs) return state;
       return { ...state, data: [...state.data, { ...action.payload }] };
     }
-    case REMOVE_FROM_FAVORITES: {
+    case FAVORITES_REMOVE_ITEM: {
       return {
-        ...state, //! action.payload is id!
+        ...state,
         data: state.data.filter(item => item._id !== action.payload),
       };
     }
-    case CLEAR_FAVORITES: {
-      //! NO action.payload !
+    case FAVORITES_CLEAR: {
       return { ...state, data: [] };
     }
     default:
