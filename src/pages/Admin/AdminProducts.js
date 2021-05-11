a0817cb59a1f1c24d6a8467706fad9f6a04bff60
Alexey Kudryashov <a.kudr74@gmail.com>
2021-05-11 20:00:48 +0300

    AdminPanel Products v.2.0 using CSS Grid

--- src/pages/Admin/AdminProducts.js
+++ src/pages/Admin/AdminProducts.js
@@ -20,49 +20,78 @@ const AdminProducts = () => {
   const deleteHandler = () => {
     /// TODO: dispatch delete action
   };
+  const productItems = products.map(product => (
+    <li key={product._id} className={styles.productItem}>
+      <div>{product._id}</div>
+      <div>{product.name}</div>
+      <div>{product.currentPrice}</div>
+      <div>{product.categories}</div>
+      <div>{product.brand}</div>
+      <div>
+        <Button
+          type='button'
+          title='Edit'
+          className={styles.action_btn}
+          onClick={() => history.push(`/product/${product._id}/edit`)}
+        />
+        <Button type='button' title='Delete' className={styles.action_btn} onClick={() => deleteHandler(product)} />
+      </div>
+    </li>
+  ));
   return (
     <PageContainer>
       <h1>Products</h1>
       {productsLoading ? (
         <Loader />
       ) : (
-        <table className={styles.table}>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>NAME</th>
-              <th>PRICE</th>
-              <th>CATEGORY</th>
-              <th>BRAND</th>
-              <th>ACTIONS</th>
-            </tr>
-          </thead>
-          <tbody>
-            {products.map(product => (
-              <tr key={product._id}>
-                <td>{product._id}</td>
-                <td>{product.name}</td>
-                <td>{product.currentPrice}</td>
-                <td>{product.categories}</td>
-                <td>{product.brand}</td>
-                <td>
-                  <Button
-                    type='button'
-                    title='Edit'
-                    className={styles.small}
-                    onClick={() => history.push(`/product/${product._id}/edit`)}
-                  />
-                  <Button
-                    type='button'
-                    title='Delete'
-                    className={styles.small}
-                    onClick={() => deleteHandler(product)}
-                  />
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <ul className={styles.grid_layout}>
+          <li>
+            <h3>ID</h3>
+            <h3>NAME</h3>
+            <h3>PRICE</h3>
+            <h3>CATEGORY</h3>
+            <h3>BRAND</h3>
+            <h3>ACTIONS</h3>
+          </li>
+          {productItems}
+        </ul>
+        // <table className={styles.table}>
+        //   <thead>
+        //     <tr>
+        //       <th>ID</th>
+        //       <th>NAME</th>
+        //       <th>PRICE</th>
+        //       <th>CATEGORY</th>
+        //       <th>BRAND</th>
+        //       <th>ACTIONS</th>
+        //     </tr>
+        //   </thead>
+        //   <tbody>
+        //     {products.map(product => (
+        //       <tr key={product._id}>
+        //         <td>{product._id}</td>
+        //         <td>{product.name}</td>
+        //         <td>{product.currentPrice}</td>
+        //         <td>{product.categories}</td>
+        //         <td>{product.brand}</td>
+        //         <td>
+        //           <Button
+        //             type='button'
+        //             title='Edit'
+        //             className={styles.small}
+        //             onClick={() => history.push(`/product/${product._id}/edit`)}
+        //           />
+        //           <Button
+        //             type='button'
+        //             title='Delete'
+        //             className={styles.small}
+        //             onClick={() => deleteHandler(product)}
+        //           />
+        //         </td>
+        //       </tr>
+        //     ))}
+        //   </tbody>
+        // </table>
       )}
     </PageContainer>
   );
