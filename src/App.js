a13f8443a2d0ed6ee7c7035dcd517c564e1910fa
Alexey Kudryashov <a.kudr74@gmail.com>
2021-05-19 12:19:53 +0300

    Breadcrumbs got fresh pull from dev

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import AppRoutes from './routes/AppRoutes';
-import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 import { checkAuthOperation } from './store/customer/operations';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   const dispatch = useDispatch();
