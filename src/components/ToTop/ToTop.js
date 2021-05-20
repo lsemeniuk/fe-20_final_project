a64e2719137b5082cee13274e64f36036212f9a0
Alexey Kudryashov <a.kudr74@gmail.com>
2021-05-20 15:39:00 +0300

    created ToTop btn

--- src/components/ToTop/ToTop.js
+++ src/components/ToTop/ToTop.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Chevron from '../../theme/icons/Chevron';
+import styles from './ToTop.module.scss';
 
-const ToTop = () => {
-  return <div>ToTop button</div>;
+const ToTopButton = () => {
+  const [showScroll, setShowScroll] = useState(false);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+  const checkScrollTop = () => {
+    if (!showScroll && window.pageYOffset > 500) {
+      setShowScroll(true);
+    } else if (showScroll && window.pageYOffset <= 500) {
+      setShowScroll(false);
+    }
+  };
+  window.addEventListener('scroll', checkScrollTop);
+
+  return (
+    <div>
+      {showScroll ? (
+        <button type='button' className={styles.scrollTop} onClick={scrollToTop}>
+          <div>
+            <Chevron />
+          </div>
+        </button>
+      ) : (
+        <button type='button' className={`${styles.scrollTop} ${styles.hidden}`} onClick={scrollToTop}>
+          <div>
+            <Chevron />
+          </div>
+        </button>
+      )}
+    </div>
+  );
 };
 
-export default ToTop;
+export default ToTopButton;
