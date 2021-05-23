c0a9ca9f725322a3c29b336c39adb52cb262a04c
Timur-er <69920009+Timur-er@users.noreply.github.com>
2021-05-23 09:31:00 +0300

    Favourites for unauthorized user (#45)
    * fav items for unauthorized user
    
    * fav items for unauthorized user
    
    * add favItems to localStorage and redux
    
    * wish list to db
    
    * wish list to db done
    
    * wish list to db done
    
    * wish list done
    
    * wish list fix
    
    * fixed local stor operation wish-list
    
    * wishList page for unauthorized user
    
    * wishList page for unauthorized user
    
    * wishList done
    
    * fix wishList button in product page
    
    Co-authored-by: Leonid Semeniuk <leo.sam@ukr.net>

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,7 +2,7 @@
 import React, { useEffect } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { INDEX_ROUTE, PRODUCTS_ROUTE, WISH_LIST_ROUTE } from '../../utils/consts';
+import { INDEX_ROUTE, PRODUCTS_ROUTE, WISH_LIST_ROUTE, CUSTOMER_WISH_LIST_ROUTE } from '../../utils/consts';
 import Container from '../Container/Container';
 import Icons from '../Icons/Icons';
 import MyOrders from './MyOrders/MyOrders';
@@ -14,32 +14,38 @@ import { getModalAuthRegSelector } from '../../store/modal/selectors';
 import { saveModalAuthRegAction } from '../../store/modal/actions';
 import { getCustomerIsAuthSelector } from '../../store/customer/selectors';
 import { getWishListSelector, wishListLoadingSelector } from '../../store/wishList/selectors';
-import { getWishListOperation } from '../../store/wishList/operations';
+import { getWishListOperation, updateWishListOperation } from '../../store/wishList/operations';
 import styles from './NavBar.module.scss';
 import { getProductsOperation } from '../../store/products/operations';
 import { getCartOperation } from '../../store/cart/operations';
+import { wishListLoadingAction } from '../../store/wishList/actions';
 
 const NavBar = () => {
   const dispatch = useDispatch();
   const modalAuthReg = useSelector(getModalAuthRegSelector);
   const isAuth = useSelector(getCustomerIsAuthSelector);
-
   const wishList = useSelector(getWishListSelector);
   const wishListLoading = useSelector(wishListLoadingSelector);
   const location = useLocation();
 
+  const storageWishList = { products: JSON.parse(localStorage.getItem('WishList')) || [] };
+
   let favorites = 0;
-  if (!wishListLoading) {
-    if (wishList) {
+  if (isAuth) {
+    if (!wishListLoading && wishList) {
       favorites = wishList.products.length;
     }
+  } else {
+    favorites = wishList !== null ? wishList.length || 0 : 0;
   }
 
   useEffect(() => {
     dispatch(getProductsOperation());
+    dispatch(wishListLoadingAction(true));
     if (isAuth) {
-      dispatch(getCartOperation());
       dispatch(getWishListOperation());
+      dispatch(updateWishListOperation(storageWishList));
+      dispatch(getCartOperation());
     }
   }, [isAuth]);
 
@@ -80,7 +86,7 @@ const NavBar = () => {
               <ul className={styles.iconList}>
                 <li key='wishList'>
                   {wishList && favorites !== 0 ? (
-                    <NavLink to={WISH_LIST_ROUTE}>
+                    <NavLink to={isAuth ? WISH_LIST_ROUTE : CUSTOMER_WISH_LIST_ROUTE}>
                       {heartJsx}
                       <span className={styles.favorites}>{favorites}</span>
                     </NavLink>
