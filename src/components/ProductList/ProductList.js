ac29592e26edc02499a7a6016e0080948058b911
Alexey Kudryashov <a.kudr74@gmail.com>
2021-05-24 19:25:05 +0300

    Filtering by Category now works, too!

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 /* eslint-disable max-len */
 import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router';
 import axios from 'axios';
 import Pagination from '../Pagination/Pagination';
 // import { useDispatch, useSelector } from 'react-redux';
@@ -8,6 +9,7 @@ import ProductCard from '../ProductCard/ProductCard';
 // import { getProductsSelector, productsLoadingSelector } from '../../store/products/selectors';
 import style from './ProductList.module.scss';
 import Loader from '../Loader/Loader';
+
 // import { getProductsOperation } from '../../store/products/operations';
 
 const ProductList = () => {
@@ -19,17 +21,29 @@ const ProductList = () => {
   const [productsLoading, setProductsLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [productsPerPage] = useState(6);
+  const { pathname } = useLocation();
+  const category = pathname.split('/')[2];
 
   useEffect(() => {
     setProductsLoading(true);
-    axios(
-      `https://fe-20-final-project.herokuapp.com/api/products/filter?perPage=${productsPerPage}&startPage=${currentPage}`
-    ).then(({ data }) => {
-      setProducts(data.products);
-      setProductsQuantity(data.productsQuantity);
-      setProductsLoading(false);
-    });
-  }, [currentPage, productsPerPage]);
+    if (category === 'all') {
+      axios(
+        `https://fe-20-final-project.herokuapp.com/api/products/filter?perPage=${productsPerPage}&startPage=${currentPage}`
+      ).then(({ data }) => {
+        setProducts(data.products);
+        setProductsQuantity(data.productsQuantity);
+        setProductsLoading(false);
+      });
+    } else {
+      axios(
+        `https://fe-20-final-project.herokuapp.com/api/products/filter?categories=${category}&perPage=${productsPerPage}&startPage=${currentPage}`
+      ).then(({ data }) => {
+        setProducts(data.products);
+        setProductsQuantity(data.productsQuantity);
+        setProductsLoading(false);
+      });
+    }
+  }, [currentPage, productsPerPage, category]);
 
   if (productsLoading) {
     return <Loader />;
@@ -45,7 +59,11 @@ const ProductList = () => {
         setCurrentPage={setCurrentPage}
         currentPage={currentPage}
       />
-      <ul className={style.productsList}>{productList}</ul>
+      {productsQuantity > 0 ? (
+        <ul className={style.productsList}>{productList}</ul>
+      ) : (
+        <h1>Продуктов в категории {category} пока нет </h1>
+      )}
     </>
   );
 };
