2f42984c0ac4c32817a4b8d1e80c9e263fa2db23
Erkimbaev Timur <etimur808@gmail.com>
2021-05-24 21:59:35 +0300

    smoke test wishlistBtn

--- src/components/AddToWishListButton/wishListBtn.test.js
+++ src/components/AddToWishListButton/wishListBtn.test.js
@@ -1,5 +1,19 @@
-// describe('Test wish list btn component', () => {
-//     test('smoke wish list btn test'), () => {
-//
-//     }
-// })
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import AddToWishListBtn from './AddToWishListBtn';
+import store from '../../store/store';
+
+const id = '60a33e74f7a92e0004be1e53';
+const itemNo = '949618';
+
+describe('Add to wishList btn test', () => {
+  test('add to wish list btn smoke test', () => {
+    render(
+      <Provider store={store}>
+        <AddToWishListBtn id={id} itemNo={itemNo} />
+      </Provider>
+    );
+  });
+});
