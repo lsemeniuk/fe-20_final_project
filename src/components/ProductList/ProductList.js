bf95378b0e920c9b67a6d39e0403752c5ba137b2
Alexey Kudryashov <a.kudr74@gmail.com>
2021-05-25 15:58:07 +0300

    delete comments

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,26 +1,18 @@
-/* eslint-disable no-console */
 /* eslint-disable max-len */
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router';
 import axios from 'axios';
 import Pagination from '../Pagination/Pagination';
-// import { useDispatch, useSelector } from 'react-redux';
 import ProductCard from '../ProductCard/ProductCard';
-// import { getProductsSelector, productsLoadingSelector } from '../../store/products/selectors';
 import style from './ProductList.module.scss';
 import Loader from '../Loader/Loader';
 
-// import { getProductsOperation } from '../../store/products/operations';
-
 const ProductList = () => {
-  // const dispatch = useDispatch();
-  // const products = useSelector(getProductsSelector);
-  // const productsLoading = useSelector(productsLoadingSelector);
   const [products, setProducts] = useState([]);
   const [productsQuantity, setProductsQuantity] = useState('');
   const [productsLoading, setProductsLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [productsPerPage, setProductsPerPage] = useState(6);
+  const [productsPerPage, setProductsPerPage] = useState(9);
   const { pathname } = useLocation();
   const category = pathname.split('/')[2];
 
@@ -53,17 +45,19 @@ const ProductList = () => {
 
   return (
     <>
-      <Pagination
-        productsPerPage={productsPerPage}
-        setProductsPerPage={setProductsPerPage}
-        totalProducts={+productsQuantity}
-        setCurrentPage={setCurrentPage}
-        currentPage={currentPage}
-      />
       {productsQuantity > 0 ? (
-        <ul className={style.productsList}>{productList}</ul>
+        <div>
+          <Pagination
+            productsPerPage={productsPerPage}
+            setProductsPerPage={setProductsPerPage}
+            totalProducts={+productsQuantity}
+            setCurrentPage={setCurrentPage}
+            currentPage={currentPage}
+          />
+          <ul className={style.productsList}>{productList}</ul>
+        </div>
       ) : (
-        <h1>Продуктов в категории {category} пока нет </h1>
+        <h1 className={style.productAbsent}>Продуктов в категории {category} пока нет</h1>
       )}
     </>
   );
