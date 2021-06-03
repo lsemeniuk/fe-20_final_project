15a16ee5310b340c1875f1cb270faa22a924216d
Erkimbaev Timur <etimur808@gmail.com>
2021-06-03 23:15:25 +0300

    testing add to cart btn

---
+++ src/components/AddToCartButton/AddToCartBtn.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import AddToCartButton from './AddToCartButton';
+import store from '../../store/store';
+
+const id = '60a33e74f7a92e0004be1e53';
+const currentPrice = 5000;
+
+describe('Add to cart btn test', () => {
+  test('Add to cart button smoke test', () => {
+    render(
+      <Provider store={store}>
+        <AddToCartButton id={id} currentPrice={currentPrice} />
+      </Provider>
+    );
+  });
+});
