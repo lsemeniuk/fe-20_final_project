4a2174f2f671b5e523a00626f67a76e7bd1d619a
Erkimbaev Timur <etimur808@gmail.com>
2021-06-09 11:35:58 +0300

    add tests

--- src/components/AddToCartButton/AddToCartBtn.test.js
+++ src/components/AddToCartButton/AddToCartBtn.test.js
@@ -4,6 +4,7 @@ import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import AddToCartButton from './AddToCartButton';
 import store from '../../store/store';
+import { product } from '../../../mokfiles/testingMok';
 
 const id = '60a33e74f7a92e0004be1e53';
 const currentPrice = 5000;
@@ -12,7 +13,7 @@ describe('Add to cart btn test', () => {
   test('Add to cart button smoke test', () => {
     render(
       <Provider store={store}>
-        <AddToCartButton id={id} currentPrice={currentPrice} />
+        <AddToCartButton product={product} id={id} currentPrice={currentPrice} />
       </Provider>
     );
   });
