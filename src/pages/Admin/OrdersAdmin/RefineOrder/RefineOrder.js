5c4ada10df435de53aaa7ed31f3130847c43899a
Leonid Semeniuk <66006891+LeoSame@users.noreply.github.com>
2021-06-09 06:43:22 +0300

    Orders list (#64)
    * add ordersList admin
    
    * add change status orders
    
    * fixed logic orders list add filterorders
    
    * add pagination in orders list

---
+++ src/pages/Admin/OrdersAdmin/RefineOrder/RefineOrder.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RefineOrder = () => {
+  return <div>dasdas</div>;
+};
+
+export default RefineOrder;
--- src/pages/CheckoutCart/PaymentDataInputs/PaymentDataInputs.js
+++ src/pages/CheckoutCart/PaymentDataInputs/PaymentDataInputs.js
@@ -4,14 +4,12 @@ import MySelect from '../../../components/Forms/MySelect/MySelect';
 const PaymentDataInputs = () => {
   return (
     <div>
-      <h3 className='checkout__title'>Оплата</h3>
       <MySelect label='Способ оплаты' name='paymentInfo'>
         <option>Выберите способ оплаты</option>
         <option value='Наличными'>Наличными</option>
         <option value='Наложенный платеж'>Наложенный платеж</option>
         <option value='Оплатить через Visa/Mastercard'>Оплатить через Visa/Mastercard</option>
       </MySelect>
-      <hr />
     </div>
   );
 };
