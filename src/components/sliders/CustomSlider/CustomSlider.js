072b6a1fcb2a8cabc8f855a409a937e842c0c616
Leonid Semeniuk <leo.sam@ukr.net>
2021-06-09 18:05:26 +0300

    add filter product sliders

--- src/components/sliders/CustomSlider/CustomSlider.js
+++ src/components/sliders/CustomSlider/CustomSlider.js
@@ -1,40 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Slider from 'react-slick';
 import ProductCard from '../../ProductCard/ProductCard';
+import Loader from '../../Loader/Loader';
+import { getProductsFilterParams } from '../../../http/productAPI';
 import styles from './CustomSlider.module.scss';
 
-const CustomSlider = ({ title, products }) => {
-  const sliderSettings = {
-    dots: false,
-    infinite: true,
-    speed: 700,
-    slidesToShow: 5,
-    slidesToScroll: 2,
-    responsive: [
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 4,
-        },
+const sliderSettings = {
+  dots: false,
+  infinite: false,
+  speed: 700,
+  slidesToShow: 5,
+  slidesToScroll: 2,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 4,
       },
-      {
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 3,
-        },
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
       },
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 2,
-          dots: true,
-          arrows: false,
-          slidesToScroll: 1,
-        },
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 2,
+        dots: true,
+        arrows: false,
+        slidesToScroll: 1,
       },
-    ],
-  };
+    },
+  ],
+};
+
+const CustomSlider = ({ title, filter }) => {
+  const [products, setProducts] = useState([]);
+  const [productsLoading, setProductLoading] = useState(true);
+
+  useEffect(() => {
+    getProductsFilterParams(filter).then(res => {
+      setProducts(res.data.products);
+      setProductLoading(false);
+    });
+  }, []);
+
+  if (productsLoading) {
+    return <Loader />;
+  }
 
   const productsList = products.map(product => <ProductCard inSlider key={product.itemNo} product={product} />);
 
@@ -52,7 +68,7 @@ const CustomSlider = ({ title, products }) => {
 
 CustomSlider.propTypes = {
   title: PropTypes.string.isRequired,
-  products: PropTypes.arrayOf(PropTypes.object).isRequired,
+  filter: PropTypes.object.isRequired,
 };
 
 export default CustomSlider;
