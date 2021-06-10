48383e6c84eab42d2aec744c019ee5fd02f3432f
Timur-er <69920009+Timur-er@users.noreply.github.com>
2021-06-10 14:15:48 +0300

    Orders (#66)
    * order page
    
    * order page fix
    
    * order page fix

--- src/pages/User/Orders/Orders.js
+++ src/pages/User/Orders/Orders.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ContainerPage from '../../../components/ContainerPage/ContainerPage';
+import { getCustomerOrders } from '../../../http/ordersAPI';
+import Order from './Order/Order';
 
 const Orders = () => {
-  return <ContainerPage>Orders</ContainerPage>;
+  const [orders, setOrders] = useState();
+
+  useEffect(() => {
+    getCustomerOrders().then(res => {
+      if (res.data.length === 0) {
+        setOrders(null);
+      } else {
+        setOrders(res.data);
+      }
+    });
+  }, []);
+
+  const ordersWrapper = orders ? (
+    orders.map(order => {
+      return <Order order={order} />;
+    })
+  ) : (
+    <div>У вас пока нет заказов</div>
+  );
+
+  return (
+    <ContainerPage>
+      <div>
+        <h2>Заказы</h2>
+      </div>
+
+      <div>{ordersWrapper}</div>
+    </ContainerPage>
+  );
 };
 
 export default Orders;
