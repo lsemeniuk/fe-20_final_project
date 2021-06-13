2bf2f0b637da3b513e3790dbc8377791036a8ee9
imperor <imperor.oktov@gmail.com>
2021-06-13 19:19:34 +0300

    del_user_column_left

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Switch, Route } from 'react-router-dom';
-import AsideBar from '../components/AsideBar/AsideBar';
-
 import Container from '../components/Container/Container';
 import { userRoutes } from './routes';
-import { PERSONAL_INFO_ROUTE, ORDERS_ROUTE, WISH_LIST_ROUTE } from '../utils/consts';
 import { getCustomerIsAuthSelector, getCustomerIsLoadingSelector } from '../store/customer/selectors';
 import Loader from '../components/Loader/Loader';
 import Page404 from '../pages/Page404/Page404';
-
-const links = [
-  { url: PERSONAL_INFO_ROUTE, description: 'Личные данные' },
-  { url: ORDERS_ROUTE, description: 'Заказы' },
-  { url: WISH_LIST_ROUTE, description: 'Список желаний' },
-];
 
 const UserRoutes = () => {
   const isAuth = useSelector(getCustomerIsAuthSelector);
@@ -27,8 +18,7 @@ const UserRoutes = () => {
   return (
     <main>
       <Container>
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-          <AsideBar links={links} />
+        <div>
           <Switch>
             {userRoutes.map(({ path, Component }) => (
               <Route key={path} path={path} component={Component} exact />
