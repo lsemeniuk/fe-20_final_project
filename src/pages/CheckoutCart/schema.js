90209ad45c5e83f6d3da5e93664c9ecc9833ce6d
mariyaudalova <61234374+mariyaudalova@users.noreply.github.com>
2021-06-14 10:12:57 +0300

    Fix project list (#76)
    * bug fix
    
    * информация о пользователе
    
    * fixNaming
    
    * fix validation
    
    * fix APK case
    
    * remove console log
    
    * fix naming
    
    * fix when array is Empty
    
    * fix words
    
    Co-authored-by: Mariia Udalova <ASCHA222011@gmail.com>
    Co-authored-by: Leonid Semeniuk <66006891+LeoSame@users.noreply.github.com>

--- src/pages/CheckoutCart/schema.js
+++ src/pages/CheckoutCart/schema.js
@@ -9,7 +9,7 @@ const schema = Yup.object().shape({
     .min(2, 'Извените, этого маловато для фамилии')
     .max(25, 'Возможно немного сократить?')
     .required('Укажите Вашу фамилию'),
-  mobile: Yup.string().matches(/^\+380\d{3}\d{2}\d{2}\d{2}$/, 'Некоректный номер телефона'),
+  mobile: Yup.string().matches(/^\+380\d{3}\d{2}\d{2}\d{2}$/, 'Некорректный номер телефона'),
   email: Yup.string().email('Неверный адрес email').required('Укажите email'),
   region: Yup.string().min(3, 'Выберите область').max(40, 'Макс. 40 букв').required('Укажите область'),
   city: Yup.string().min(3, 'Выберите город').max(60, 'Макс. 60 букв').required('Укажите город'),
