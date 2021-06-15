7a69e31e009d8ad6c78dabdeb5bdfaa66b21f3e5
Leonid Semeniuk <leo.sam@ukr.net>
2021-06-16 00:20:07 +0300

    fixed styles, remove comments

--- src/pages/Product/ProductColors/ProductColors.js
+++ src/pages/Product/ProductColors/ProductColors.js
@@ -32,7 +32,7 @@ const ProductColors = ({ color, descForColor }) => {
         return (
           <li
             key={product.itemNo}
-            className={color === product.color ? `${styles.item} ${styles.colorActive}` : styles.item}
+            className={color === product.color ? `${styles.item} ${styles.itemActive}` : styles.item}
           >
             <NavLink to={`${PRODUCT_ROUTE}/${product.itemNo}`}>
               <div style={{ borderColor: colors[i].cssValue }} className={styles.border}>
@@ -47,33 +47,6 @@ const ProductColors = ({ color, descForColor }) => {
     }
     return null;
   });
-
-  // const colorList = colors.map(col => {
-
-  //   if (col.name === color) {
-  //     return (
-  //       <li key={col.name} className={`${styles.item} ${styles.colorActive}`}>
-  //         <div style={{ borderColor: col.cssValue }} className={styles.border}>
-  //           <div style={{ backgroundColor: col.cssValue }} className={styles.color}>
-  //             {}
-  //           </div>
-  //         </div>
-  //       </li>
-  //     );
-  //   }
-  //   if (col.name === 'bronze') {
-  //     return (
-  //       <li style={{ borderColor: col.cssValue }} key={col.name} className={`${styles.item}`}>
-  //         <div style={{ borderColor: col.cssValue }} className={styles.border}>
-  //           <div style={{ backgroundColor: col.cssValue }} className={styles.color}>
-  //             {}
-  //           </div>
-  //         </div>
-  //       </li>
-  //     );
-  //   }
-  //   return null;
-  // });
 
   return (
     <div className={styles.container}>
