6d00aeaaa72fa09c965e65be11d02b1695380b71
Erkimbaev Timur <etimur808@gmail.com>
2021-06-17 23:47:38 +0300

    adaptive slider & product cart

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import AppRoutes from './routes/AppRoutes';
-// import NavBar from './components/NavBar/NavBar';
+import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 import { checkAuthOperation } from './store/customer/operations';
 import ToTopButton from './components/ToTop/ToTop';
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <div className='App'>
-      {/* <NavBar /> */}
+      <NavBar />
       <ScrollToTop />
       <AppRoutes />
       <Footer />
