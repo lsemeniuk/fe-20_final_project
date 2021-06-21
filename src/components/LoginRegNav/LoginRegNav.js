3bca2f8c5f88b5182153ea567405eafa6b61f7c2
Alexey Kudryashov <a.kudr74@gmail.com>
2021-06-21 18:23:25 +0300

    work in progress on AuthReg mobile form

--- src/components/LoginRegNav/LoginRegNav.js
+++ src/components/LoginRegNav/LoginRegNav.js
@@ -1,64 +1,49 @@
 /* eslint-disable no-console */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import AuthForm from '../RegAuth/AuthForm/AuthForm';
+import RegForm from '../RegAuth/RegForm/RegForm';
+
 import styles from './LoginRegNav.module.scss';
-import { PRODUCTS_ROUTE } from '../../utils/consts';
 import Icons from '../Icons/Icons';
-import menWatch from '../../theme/img/men-watch.webp';
-import womenWatch from '../../theme/img/women-watch.webp';
-import kidsWatch from '../../theme/img/kids-watch.webp';
-import accessories from '../../theme/img/accessories-pic.webp';
 
 const LoginRegNav = ({ showLogin, toggleLoginReg }) => {
-  // const isAuth = useSelector(getCustomerIsAuthSelector);
-  // const wishList = useSelector(getWishListSelector);
-
-  // const cartLocal = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : {};
-  // const cartQuantity = cartLocal !== {} ? cartLocal.products.length : 0;
-
+  const [tabIndex, setTabIndex] = useState(0);
+  const [messageServer, setmessageServer] = useState(null);
+  const [forgotOpen, setForgotOpen] = useState(false);
+  const setTabIndexToReg = () => {
+    setmessageServer(
+      <span style={{ color: 'green' }}>
+        Поздравляем, Вы зарегистрированы <br />А теперь введите свои email и пароль
+      </span>
+    );
+    setTabIndex(0);
+  };
   return (
     <aside className={showLogin ? `${styles.container} ${styles.active}` : `${styles.container}`}>
-      <div className={styles.header}>
+      <div className={styles.form__container}>
         <div className={styles.icon__container} onClick={toggleLoginReg}>
           <Icons type='close' width={35} height={35} />
         </div>
-        <h2 className={styles.title}>Каталог</h2>
-      </div>
-      <div className={styles.menu} onClick={toggleLoginReg}>
-        <div className={styles.menu__wrapper}>
-          <ul className={styles.menu__slideOut}>
-            <li className={styles.navItem}>
-              <img className={styles.navIcon} src={menWatch} alt='men watch' />
-              <NavLink to={`${PRODUCTS_ROUTE}/men`}>
-                <p className={styles.navLabel}>Мужские</p>
-              </NavLink>
-            </li>
-            <li className={styles.navItem}>
-              <img className={styles.navIcon} src={womenWatch} alt='women watch' />
-              <NavLink to={`${PRODUCTS_ROUTE}/women`}>
-                <p className={styles.navLabel}>Женские</p>
-              </NavLink>
-            </li>
-            <li className={styles.navItem}>
-              <img className={styles.navIcon} src={kidsWatch} alt='kids watch' />
-              <NavLink to={`${PRODUCTS_ROUTE}/kids`}>
-                <p className={styles.navLabel}>Детские</p>
-              </NavLink>
-            </li>
-            <li className={styles.navItem}>
-              <img className={styles.navIcon} src={accessories} alt='accessories' />
-              <NavLink to={`${PRODUCTS_ROUTE}/accessories`}>
-                <p className={styles.navLabel}>Аксессуары</p>
-              </NavLink>
-            </li>
-            <li className={styles.navItem}>
-              <img className={styles.hidden} src={accessories} alt='accessories' />
-              <div className={styles.navLabel}>Бренды</div>
-            </li>
-          </ul>
-        </div>
+        <Tabs selectedIndex={tabIndex} onSelect={index => setTabIndex(index)}>
+          <TabList>
+            <Tab tabIndex='0'>Вход</Tab>
+            <Tab tabIndex='0'>Регистрация</Tab>
+          </TabList>
+          <TabPanel>
+            <div className={styles.form}>
+              <div className={styles.redTitle}>{messageServer}</div>
+              <AuthForm setmessageServer={setmessageServer} setForgotOpen={setForgotOpen} forgotOpen={forgotOpen} />
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <div className={styles.form}>
+              <RegForm setTabIndex={setTabIndexToReg} />
+            </div>
+          </TabPanel>
+        </Tabs>
       </div>
     </aside>
   );
