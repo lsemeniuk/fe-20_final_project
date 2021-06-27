7ba77ac81220f4587d960b97fa387413c43074cf
Leonid Semeniuk <leo.sam@ukr.net>
2021-06-27 04:12:29 +0300

    first

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -34,10 +34,10 @@ const Product = () => {
     const viwedProducts = JSON.parse(localStorage.getItem('viwed_products'));
 
     if (viwedProducts && viwedProducts.length >= 1) {
-      const newViwedProducts = [...new Set([params.id, ...viwedProducts])];
+      const newViwedProducts = [...new Set([params.productUrl, ...viwedProducts])];
       localStorage.setItem('viwed_products', JSON.stringify(newViwedProducts));
     } else {
-      localStorage.setItem('viwed_products', JSON.stringify([params.id]));
+      localStorage.setItem('viwed_products', JSON.stringify([params.productUrl]));
     }
   }, [params.productUrl]);
 
