c6a2abb287f5e97d87f720c7f19b13ba655a040b
Erkimbaev Timur <etimur808@gmail.com>
2021-06-28 21:43:53 +0300

    add icons

--- src/components/CartList/CartList.test.js
+++ src/components/CartList/CartList.test.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import CartList from './CartList';
+import store from '../../store/store';
 
 describe('Cart list test', () => {
-  test('Cart list smoke test', () => {});
+  test('Cart list smoke test', () => {
+    render(
+      <Provider store={store}>
+        <CartList />
+      </Provider>
+    );
+  });
 });
