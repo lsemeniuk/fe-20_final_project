248531d8dc9776fdb993bfb23f6051b4d1827fc2
Leonid Semeniuk <leo.sam@ukr.net>
2021-06-28 19:05:21 +0300

    add footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 // import AppRoutes from './routes/AppRoutes';
 import NavBar from './components/NavBar/NavBar';
-// import Footer from './components/Footer/Footer';
+import Footer from './components/Footer/Footer';
 import { checkAuthOperation } from './store/customer/operations';
 // import ToTopButton from './components/ToTop/ToTop';
 import ScrollToTop from './components/ScrollToTop/ScrollToTop';
@@ -19,9 +19,9 @@ function App() {
       <NavBar />
       <ScrollToTop />
       <div style={{ height: '100vh' }}>{}</div>
-      {/* <AppRoutes />
+      {/* <AppRoutes /> */}
       <Footer />
-      <ToTopButton /> */}
+      {/* <ToTopButton /> */}
     </div>
   );
 }
