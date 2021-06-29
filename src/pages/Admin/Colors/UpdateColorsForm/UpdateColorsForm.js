1677352bfe9ae5adad8785e4e979d5c27e245c6d
Leonid Semeniuk <leo.sam@ukr.net>
2021-06-29 14:13:12 +0300

    add images in brand admin

--- src/pages/Admin/Colors/UpdateColorsForm/UpdateColorsForm.js
+++ src/pages/Admin/Colors/UpdateColorsForm/UpdateColorsForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useDispatch } from 'react-redux';
 import { Formik, Form } from 'formik';
 import PropTypes from 'prop-types';
